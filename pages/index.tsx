5a1c752b876779dcab4bb07aa6070d123272b727
motokitakumi <mtktkm242424@gmail.com>
2022-09-04 15:22:47 +0900

    configデータの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { access_token } from "../config";
+import { access_token, user_id, user_name } from "../config";
 
 const Home: NextPage = () => {
+  const url = `https://graph.facebook.com/v14.0/${user_id}?fields=business_discovery.username(${user_name}){followers_count,media_count,media{comments_count,like_count}}&access_token=${access_token}`;
+
   useEffect(() => {
-    console.log(access_token);
+    console.log(url);
   }, []);
   return (
     <>
