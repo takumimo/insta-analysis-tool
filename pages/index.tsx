ab5a4b4afe469daa83fcb722390db5d3b8cbcc3d
motokitakumi <mtktkm242424@gmail.com>
2022-09-09 18:54:53 +0900

    グラフを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,123 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { access_token, user_id, user_name } from "../config";
 import axios from "axios";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+} from "chart.js";
+import { Doughnut, Line } from "react-chartjs-2";
+import faker from 'faker';
 
 const Home: NextPage = () => {
-  const url = `https://graph.facebook.com/v14.0/${user_id}?fields=business_discovery.username(${user_name}){followers_count,media_count,follows_count}&access_token=${access_token}`;
+  ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title
+  );
 
-  const [bizData, setBizData] = useState();
+  const followerUrl = `https://graph.facebook.com/v14.0/${user_id}?fields=business_discovery.username(${user_name}){followers_count,media_count,follows_count}&access_token=${access_token}`;
+  const genderUrl = `https://graph.facebook.com/v14.0/${user_id}/insights?access_token=${access_token}&metric=audience_gender_age&period=lifetime`;
+  const [female, setFemaile] = useState();
+  const [male, setMale] = useState();
+
+  const data = {
+    labels: ["女性", "男性"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [female, male],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data2 = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
 
   useEffect(() => {
-    fetchMainData();
-  }, [bizData]);
+    const b = fetchGender();
+    console.log(b);
+  }, [male, female]);
 
-  const fetchMainData = async () => {
-    const returnData = await axios.get(url);
-    const { followers_count } = returnData.data.business_discovery;
-    setBizData(followers_count);
+  const fetchGender = async () => {
+    const returnData = await axios.get(genderUrl);
+    const a = returnData.data.data[0].values[0].value;
+    const array = Object.keys(a).map((k) => ({ key: k, value: a[k] }));
+    const regexp = /F/g;
+    const check = array.filter((v) => v.key.match(regexp));
+    // const b = Object.keys(a).filter((v) => v.match("F"));
+    // const c = a.filter(v => v.keys == b)
+    const all = check.map((v) => v.value);
+    const reducer = (sum, currentValue) => sum + currentValue;
+    const FemaleAll = all.reduce(reducer);
+    setFemaile(FemaleAll); //女性の合計人数
+
+    const b = returnData.data.data[0].values[0].value;
+    const arrayB = Object.keys(a).map((k) => ({ key: k, value: b[k] }));
+    const regexpB = /M/g;
+    const checkB = arrayB.filter((v) => v.key.match(regexpB));
+    // const b = Object.keys(a).filter((v) => v.match("F"));
+    // const c = a.filter(v => v.keys == b)
+    const allB = checkB.map((v) => v.value);
+    const reducerB = (sumB, currentValueB) => sumB + currentValueB;
+    const maleAllB = allB.reduce(reducerB);
+    setMale(maleAllB); //男性の合計人数
   };
   return (
     <>
@@ -27,8 +130,15 @@ const Home: NextPage = () => {
         <meta name="description" content="簡易的なインスタ分析ツールです" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>{bizData}</main>
+      <div className="">
+        <p>フォロワー男女比</p>
+        <div className="text-3xl w-96 h-96">
+          <Doughnut data={data}></Doughnut>
+        </div>
+        <div>
+          <Line options={options} data={data2} />
+        </div>
+      </div>
     </>
   );
 };
